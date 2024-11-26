8e96daac5df5884c230a9fc1a92e58321d40efad
Anthony Vanoni <tony@wrestlingwithcode.com>
2024-11-26 09:32:34 -0800

    update schema to include additional tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -10,4 +10,28 @@ export default defineSchema({
     about: v.optional(v.string()),
     logo: v.optional(v.string()),
   }),
+
+  products: defineTable({
+    clerkId: v.string(),
+    name: v.string(),
+    description: v.string(),
+    content: v.optional(v.string()),
+    price: v.number(),
+    currency: v.string(),
+    coverImage: v.optional(v.string()),
+    published: v.boolean(),
+  }),
+
+  sales: defineTable({
+    storeClerkId: v.string(),
+    customerClerkId: v.string(),
+    productId: v.string(),
+    price: v.number(),
+    currency: v.string(),
+  }),
+
+  keys: defineTable({
+    clerkId: v.string(),
+    stripeKey: v.string(),
+  }),
 });
